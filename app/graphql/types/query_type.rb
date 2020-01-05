b6bdb4f192cc446388d6312310ff57d2b0af9e1f
Kelvin Tsoi <ktsoi@wearfigs.com>
2020-01-04 23:46:32 -0800

    add user type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
+require 'json'
+
+PRODUCTS = [
+  {
+    id: 1,
+    title: 'Widget 1',
+    description: 'This is description for Widget 1.',
+    user: {
+      id: 1,
+      email: 'jane.doe@example.com',
+      first_name: 'Jane',
+      last_name: 'Doe'
+    }
+  },
+  {
+    id: 2,
+    title: 'Widget 2',
+    description: 'This is a description for Widget 2',
+    user: {
+      id: 2,
+      email: 'john.doe@example.com',
+      first_name: 'John',
+      last_name: 'Doe'
+    }
+  }
+].freeze
 
 module Types
   class QueryType < Types::BaseObject
@@ -15,18 +41,13 @@ module Types
     # products query
     field :products, [Types::ProductType], null: false, description: 'Returns a list of products'
     def products
-      [
-        {
-          id: 1,
-          title: 'Widget 1',
-          description: 'This is description for Widget 1.'
-        },
-        {
-          id: 2,
-          title: 'Widget 2',
-          description: 'This is a description for Widget 2'
-        }
-      ]
+      PRODUCTS
+    end
+
+    # user query
+    field :users, [Types::UserType], null: false, description: 'Returns a list of users'
+    def users
+      PRODUCTS.map { |product| product[:user] }
     end
   end
 end
