6937229845e4c5dbdad62d23894889a791bcba8d
Kelvin Tsoi <ktsoi@wearfigs.com>
2020-01-05 09:59:11 -0800

    remove Home controller and create App controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  # Root React Apollo app
+  root "app#index"
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
