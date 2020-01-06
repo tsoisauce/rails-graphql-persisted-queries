9c74f606c2ebf84c3572b4cf562404f78cdf8723
Kelvin Tsoi <ktsoi@wearfigs.com>
2020-01-05 18:20:11 -0800

    add sample_data.rb

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,31 +1,7 @@
 # frozen_string_literal: true
 
 require 'json'
-
-PRODUCTS = [
-  {
-    id: 1,
-    title: 'Widget 1',
-    description: 'This is description for Widget 1.',
-    user: {
-      id: 1,
-      email: 'jane.doe@example.com',
-      first_name: 'Jane',
-      last_name: 'Doe'
-    }
-  },
-  {
-    id: 2,
-    title: 'Widget 2',
-    description: 'This is a description for Widget 2',
-    user: {
-      id: 2,
-      email: 'john.doe@example.com',
-      first_name: 'John',
-      last_name: 'Doe'
-    }
-  }
-].freeze
+require './sample_data.rb'
 
 module Types
   class QueryType < Types::BaseObject
